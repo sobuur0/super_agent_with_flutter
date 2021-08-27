1d112a477c2e4dc7b438626a81467198f55e7179
t-bolt <isholasobuur@gmail.com>
2021-08-27 17:16:04 +0100

    changed the fonts and updated the pubspec file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:super_agent_with_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
