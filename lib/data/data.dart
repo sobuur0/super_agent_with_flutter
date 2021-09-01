9f046195c9cff79d1c448096b8d66e57a0b33bad
t-bolt <isholasobuur@gmail.com>
2021-09-01 13:14:36 +0100

    completed the interface for the first onboarding screen

---
+++ lib/data/data.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SliderModel{
+  late String imagePath1;
+  late String imagePath2;
+  late String title;
+  late String description;
+
+  // SliderModel({ this.imagePath1, this.imagePath2, this.title, this.description});
+
+  void setImageAssetPath1(String getImagepath1) {
+    imagePath1 = getImagepath1;
+  }
+
+  void setImageAssetPath2(String getImagepath2) {
+    imagePath2 = getImagepath2;
+  }
+
+  void setTitle(String getTitle){
+    title = getTitle;
+  }
+
+  void setDesc(String getDesc){
+    description = getDesc;
+  }
+
+  String getImageImageAssetPath1(){
+    return imagePath1;
+  }
+
+  String getImageImageAssetPath2(){
+    return imagePath2;
+  }
+
+  String getTitle(){
+    return title;
+  }
+
+  String getDescription(){
+    return description;
+  }
+}
+
+List<SliderModel> getSlides () {
+  List<SliderModel> slides = <SliderModel>[];
+  SliderModel sliderModel = new SliderModel();
+
+  //first slide
+  sliderModel.setTitle('Welcome \nOnboard');
+  sliderModel.setDesc('Helgg Scooters is Located \nNear you');
+  sliderModel.setImageAssetPath1('assets/onboard1.png');
+  sliderModel.setImageAssetPath2('assets/navigation1.png');
+  slides.add(sliderModel);
+
+  //Second Slide
+
+
+  return slides;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,108 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:super_agent_with_flutter/data/data.dart';
 
 void main() =>
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MyApp());
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Onboarding testing",
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  static const List<String> _titles = [
+    'Welcome \nOnboard',
+    'It\'s a \nQuick One',
+    'Scan QR Code \nTo Ride'
+  ];
+
+  static const List<String> _descriptions = [
+    'Helgg Scooters is Located \nNear you',
+    'Pay for Every Ride Using Helgg Wallet',
+    'With Just One tap, you\'re \ngood to go'
+  ];
+
+  List<SliderModel> slides = <SliderModel>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    slides = getSlides();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset('assets/jogging.png'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text(
-          "Testing",
-          style: TextStyle(
-            color: Colors.green[100],
-            fontFamily: 'Allison',
-            letterSpacing: 2.0,
-          ),
-        ),
+      body: PageView.builder(
+        itemCount: slides.length,
+          itemBuilder: (context, index) {
+            return SliderTile(
+              imageAssetPath1: slides[index].getImageImageAssetPath1(),
+              imageAssetPath2: slides[index].getImageImageAssetPath2(),
+              title: slides[index].getTitle(),
+              descriptions: slides[index].getDescription()
+            );
+          }
+      )
+    );
+  }
+}
+
+class SliderTile extends StatelessWidget {
+
+  late String imageAssetPath1, imageAssetPath2, title, descriptions;
+  SliderTile({required this.title, required this.descriptions, required this.imageAssetPath1, required this.imageAssetPath2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Text(title, style: TextStyle(fontSize: 39, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+          SizedBox(height: 20),
+          Text(descriptions, textAlign: TextAlign.center,),
+          Image.asset(imageAssetPath1),
+          SizedBox(height: 70,),
+          Image.asset(imageAssetPath2),
+          SizedBox(height: 40,),
+          ElevatedButton(
+              onPressed: null,
+              child: Text('Let\'s Begin', style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,),
+              ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+              fixedSize: MaterialStateProperty.all(Size(250, 50)),
+              shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0),
+                    side: BorderSide(color: Colors.transparent),
+                  ))
+            ),
+            ),
+          // ElevatedButton(onPressed: onPressed, child: child),
+        ],
       ),
     );
   }
 }
+
+
+
+
 
