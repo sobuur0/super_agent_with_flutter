934dfad9e47c1e3f3fb4506ea2d37bc11c0a6167
t-bolt <isholasobuur@gmail.com>
2021-10-11 20:10:03 +0100

    added a key to hold user signUp info

--- lib/presentation/pages/sign_in_page.dart
+++ lib/presentation/pages/sign_in_page.dart
@@ -11,6 +11,7 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
   late bool _passwordVisible;
+  GlobalKey _formKey = GlobalKey();
 
   @override
   // ignore: must_call_super
@@ -25,109 +26,112 @@ class _SignInPageState extends State<SignInPage> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 100,
-                ),
-                Image.asset('assets/images/atlasImage.png'),
-                Text(
-                  'Atlas',
-                  style: AppStyles.latoBold(
-                    color: Color(0xFF263746),
-                    size: 25,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 100,
                   ),
-                ),
-                Text(
-                  'Super Agent',
-                  style: AppStyles.latoRegular(
-                    color: Color(0xFF263746),
-                  ),
-                ),
-                SizedBox(
-                  height: 90,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Color(0xFF4F4F4F),
-                      ),
+                  Image.asset('assets/images/atlasImage.png'),
+                  Text(
+                    'Atlas',
+                    style: AppStyles.latoBold(
+                      color: Color(0xFF263746),
+                      size: 25,
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextField(
-                    maxLength: 8,
-                    obscureText: !_passwordVisible,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Color(0xFF4F4F4F),
-                      ),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _passwordVisible = !_passwordVisible;
-                          });
-                        },
-                        icon: Icon(
-                          _passwordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
+                  Text(
+                    'Super Agent',
+                    style: AppStyles.latoRegular(
+                      color: Color(0xFF263746),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 90,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(
+                          Icons.email,
                           color: Color(0xFF4F4F4F),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  child: ActionButton(
-                    buttonText: 'Log In',
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SetPinPage()));
-                    },
-                    buttonColor: Color(0xFF2553CF),
+                  SizedBox(
+                    height: 60,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 90, 0, 0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Expanded(
-                        child: Image.asset('assets/images/order_confirmed.png'),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                      maxLength: 8,
+                      obscureText: !_passwordVisible,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Color(0xFF4F4F4F),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisible = !_passwordVisible;
+                            });
+                          },
+                          icon: Icon(
+                            _passwordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Color(0xFF4F4F4F),
+                          ),
+                        ),
                       ),
-                      Expanded(
-                        child: Image.asset('assets/images/fast_loading.png'),
-                      ),
-                      Expanded(
-                        child: Image.asset('assets/images/jogging.png'),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-                TextField(
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: ActionButton(
+                      buttonText: 'Log In',
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SetPinPage()));
+                      },
+                      buttonColor: Color(0xFF2553CF),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 90, 0, 0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Expanded(
+                          child: Image.asset('assets/images/order_confirmed.png'),
+                        ),
+                        Expanded(
+                          child: Image.asset('assets/images/fast_loading.png'),
+                        ),
+                        Expanded(
+                          child: Image.asset('assets/images/jogging.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  TextField(
 
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
