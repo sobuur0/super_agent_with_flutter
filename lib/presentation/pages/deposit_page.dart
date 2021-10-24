e4b6926296ea89c20d9e28e565da3099baa3d351
t-bolt <isholasobuur@gmail.com>
2021-10-24 10:08:26 +0100

    initial UI for deposit page

---
+++ lib/presentation/pages/deposit_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class DepositPage extends StatefulWidget {
+  @override
+  _DepositPageState createState() => _DepositPageState();
+}
+
+class _DepositPageState extends State<DepositPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
